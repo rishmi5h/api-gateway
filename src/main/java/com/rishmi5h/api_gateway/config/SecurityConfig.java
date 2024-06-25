dfed5d250ac30066db98dc9ed05715e615ad3921
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-06-26 01:23:36 +0530

    add(docs): added swagger for api docs and permit docs without security

--- src/main/java/com/rishmi5h/api_gateway/config/SecurityConfig.java
+++ src/main/java/com/rishmi5h/api_gateway/config/SecurityConfig.java
@@ -11,9 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
+    public final String[] freeResourceUrls = new String[]{"/swagger-ui.html", "/swagger-ui/**", "v3/api-docs/**",
+           "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-       return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+       return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                               .requestMatchers(freeResourceUrls)
+                               .permitAll()
+                               .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
